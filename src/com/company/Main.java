f33cec4e92f8e43a98350d0d3b8f35d0c04506c4
Wistis <ceo@wistis.ru>
2021-05-22 02:22:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        String string = in.nextLine();
+
+        string = string.replace(" ", "");//убираем пробелы
+        string = string.replace("\"", "");//убираем кавычку
+
+
+        String[] subStr;
+        String delimeter; // Разделитель
+
+        delimeter = "\\+";
+
+        subStr = string.split(delimeter); // Разделения строки str с помощью метода split()
+
+
+        if (subStr.length == 2) {
+            System.out.println(subStr[0] + "" + subStr[1]);
+        }
+        if (new Scanner(subStr[1]).hasNextInt()) {
+            int number = Integer.parseInt(string);
+            System.out.printf( "ЧИСЛО");
+        }else {
+            System.out.printf( "СТРОКА");
+        }
+
+
+
+/*
+
+
+*/
+
+
+        in.close();
+    }
+
+
+
+    /* String[] subStr;
+      String delimeter = "-"; // Разделитель
+      subStr = str.split(delimeter); // Разделения строки str с помощью метода split()
+      // Вывод результата на экран
+      for(int i = 0; i < subStr.length; i++) {
+         System.out.println(subStr[i]);
+      }*/
+}
